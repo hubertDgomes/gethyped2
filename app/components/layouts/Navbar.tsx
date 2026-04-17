91a5415d5fac89973f1ec78923ae108191856f3a
hubertDgomes <gomeshubert8@gmail.com>
2026-04-18 00:43:53 +0600

    Add the animation effect with framer motion

--- app/components/layouts/Navbar.tsx
+++ app/components/layouts/Navbar.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import MovingBtn from "../ui/MovingBtn";
 import { IoMdMenu } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
-
+import { motion } from "motion/react"
 
 const inter = Inter();
 
@@ -43,21 +43,70 @@ const Navbar = () => {
               className="block lg:hidden mr-[50px] text-[40px] bg-pink-300 p-3 rounded-[10px] cursor-pointer z-40">{toggle ? <IoCloseOutline /> : <IoMdMenu />}</button>
           </div>
           {toggle &&
-            <div className="absolute inset-0 m-[10px] mr-[40px] h-[100vh] bg-pink-300 z-30 pt-28 px-6 rounded-2xl lg:hidden flex flex-col p-5">
-              <ul className="flex flex-col font-bold gap-x-[10px] rounded-2xl text-[18px] text-black justify-center items-center h-screen gap-4">
+            <motion.div
+              initial={{
+                y: -100,
+                rotate: 3,
+                opacity: 0,
+              }}
+              whileInView={{
+                y: 0,
+                rotate: 0,
+                opacity: 1,
+              }}
+              viewport={{ once: true }}
+              transition={{
+                type: "spring",
+                stiffness: 200,
+                damping: 10,
+                mass: 0.8,
+              }}
+              className="absolute inset-0 m-[10px] mr-[40px] h-[100vh] bg-pink-300 z-30 pt-28 px-6 rounded-2xl lg:hidden flex flex-col p-5">
+              <motion.ul
+                initial={{
+                  y: -100,
+                  opacity: 0,
+                }}
+                whileInView={{
+                  y: 0,
+                  opacity: 1,
+                }}
+                viewport={{ once: true }}
+                transition={{
+                  type: "spring",
+                  stiffness: 200,
+                  damping: 10,
+                  mass: 0.8,
+                  delay: 0.2
+                }}
+                className="flex flex-col font-bold gap-x-[10px] rounded-2xl text-[18px] text-black justify-center items-center h-screen gap-4">
                 <MovingBtn className="bg-white rounded-[10px] w-fit text-[30px]" text="Expertises" />
                 <MovingBtn className="bg-white rounded-[10px] w-fit text-[30px]" text="Work" />
                 <MovingBtn className="bg-white rounded-[10px] w-fit text-[30px]" text="About" />
                 <MovingBtn className="bg-white rounded-[10px] w-fit text-[30px]" text="Contact" />
-              </ul>
-              <button className="sideBtn4 flex items-center gap-3 mb-[40px] m-auto">
+              </motion.ul>
+              <motion.button
+                initial={{
+                  y: 30,
+                  opacity: 0,
+                }}
+                whileInView={{
+                  y: 0,
+                  opacity: 1,
+                }}
+                viewport={{ once: true }}
+                transition={{
+                  type: "spring",
+                  stiffness: 200,
+                }}
+                className="sideBtn4 flex items-center gap-3 mb-[40px] m-auto">
                 Get Result{" "}
                 <span className="p-2 bg-white text-yellow-500 rounded-[10px]">
                   <FaFire />
                 </span>
-              </button>
+              </motion.button>
 
-            </div>
+            </motion.div>
           }
         </Container>
       </div>
